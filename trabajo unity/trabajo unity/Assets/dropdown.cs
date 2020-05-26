0a8b6a87e40cd1a3ff741ed80be596f8cf780c9f
ErichGiusseppe <63005873+ErichGiusseppe@users.noreply.github.com>
2020-05-26 11:11:08 -0500

    k

---
+++ trabajo unity/trabajo unity/Assets/dropdown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dawda : MonoBehaviour
+{
+    private Animator buttonAnim;
+    private Boton boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonAnim = GetComponent<Animator>();
+        boton = GameObject.Find("Code").GetComponent<Boton>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boton.shovmenu)
+        {
+            buttonAnim.SetBool("Mostrar_visu", true);
+        }
+
+        if (!boton.shovmenu)
+        {
+            buttonAnim.SetBool("Mostrar_visu", false);
+        }
+
+
+    }
+}
